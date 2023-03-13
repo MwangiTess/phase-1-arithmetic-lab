d968bd30fed7da5eb482f1115f0bf4f659ad1305
Teresia <teresiamwangi26@gmail.com>
2023-03-14 01:42:59 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2
+multiply;
+
+
+let random = 21;
+random;
+(Number.isInteger(random));
+
+
+let num3 = 8;
+let num4 = 2;
+
+let mod = num3/num4;
+mod;
+
+
+
+let max = 20
+//Math.max(11,9,20,1,8);
+max;
